dcc871d6cb005933793515d9252dff7a0993018a
Oleg <of@weblife.ua>
2025-07-02 17:55:36 +0300

    fix

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   initBurgerMenu();
-    initClientLogosSwiper();
+  initClientLogosSwiper();
 });
 
 function initBurgerMenu() {
@@ -17,7 +17,7 @@ function initBurgerMenu() {
     document.addEventListener("click", (event) => {
       const menuContainer = document.querySelector(".mobile-menu");
       if (
-        menuContainer && 
+        menuContainer &&
         !menuContainer.contains(event.target) &&
         menu.classList.contains("open")
       ) {
@@ -72,3 +72,57 @@ function initClientLogosSwiper() {
     allowTouchMove: false,
   });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const firstAccordionItem = document.querySelector('.accordion-item');
+  const firstAccordionContent = firstAccordionItem.querySelector('.accordion-content');
+
+  firstAccordionContent.style.display = 'block';
+  firstAccordionItem.classList.add('active');
+
+  document.querySelectorAll(".accordion-item").forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      if (window.innerWidth > 768) {
+        const content = item.querySelector(".accordion-content");
+
+        const firstContent = document.querySelector('.accordion-item .accordion-content');
+        const firstItem = document.querySelector('.accordion-item');
+        if (firstContent && firstItem) {
+          firstContent.style.display = 'none';
+          firstItem.classList.remove('active');
+        }
+
+        item.classList.add("active");
+        content.style.display = "block";
+      }
+    });
+
+    item.addEventListener("mouseleave", () => {
+      if (window.innerWidth > 768) {
+        const content = item.querySelector(".accordion-content");
+
+        item.classList.remove("active");
+        content.style.display = "none";
+      }
+    });
+
+    item.querySelector(".accordion-header").addEventListener("click", () => {
+      const content = item.querySelector(".accordion-content");
+
+      if (window.innerWidth <= 768) {
+        document.querySelectorAll(".accordion-content").forEach((content) => {
+          content.style.display = "none";
+          content.closest(".accordion-item").classList.remove("active");
+        });
+
+        if (content.style.display !== "block") {
+          content.style.display = "block";
+          item.classList.add("active");
+        }
+      }
+    });
+  });
+});
+
+
+
